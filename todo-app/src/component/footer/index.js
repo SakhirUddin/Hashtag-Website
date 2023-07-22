31e09f27f4f5da9633f96a15c269c7f2d758f6e3
Shubham <shubhamitd237@gmail.com>
2023-07-22 20:05:09 +0530

    added code changes

---
+++ todo-app/src/component/footer/index.js
@@ -0,0 +1,8 @@
+import './index.css';
+const Footer = () => {
+  return (
+    <div className="footer"/>
+  );
+};
+
+export default Footer;
---
+++ todo-app/src/container/main/index.js
@@ -0,0 +1,14 @@
+
+import Footer from "../../component/footer";
+import Header from "../../component/header";
+
+const main =()=>{
+    return (
+      <>
+        <Header />
+        <Footer />
+      </>
+    );
+}
+
+export default main;
