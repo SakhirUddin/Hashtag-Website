19d8776655ddd80e507504c76965ecec4e013a44
TheArcher10 <105221958+TheArcher10@users.noreply.github.com>
2023-11-25 17:28:58 +0530

    First edit

--- e-commerce/src/App.js
+++ e-commerce/src/App.js
@@ -1,10 +1,12 @@
 import Header from './Header/header';
+import Toolbar from './Toolbar/navbar';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
      <Header/>
+     <Toolbar/>
     </div>
   );
 }
